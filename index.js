d51bd50f7494c2bc0b3ed7d71f22149579c6bf2f
elishakaranja <145563815+elishakaranja@users.noreply.github.com>
2023-10-17 20:01:13 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+const mainElement = document.getElementById("main");
+mainElement.remove()   //main removed
+
+
+
+const newHeader = document.createElement("h1")
+newHeader.id="victory"//giving header an id of victory
+
+newHeader.textContent = "YOUR-NAME is the champion"
+
+const element = document.body
+element.appendchild(newHeader)
